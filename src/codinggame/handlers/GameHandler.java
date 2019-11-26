8e71edac8639f44c248737d49798dc5cd7be9466
Welcome <Welcome@Welcome-PC>
2019-11-27 06:29:15 +0700

    Much Better Entity System

--- src/codinggame/handlers/GameHandler.java
+++ src/codinggame/handlers/GameHandler.java
@@ -1,9 +1,11 @@
 package codinggame.handlers;
 
+import codinggame.CodingGame;
 import codinggame.globjs.Light;
 import codinggame.map.GameMap;
 import codinggame.map.MapTile;
 import codinggame.map.MapTilesets;
+import codinggame.map.proceduralmap.ChunkGenerator;
 import codinggame.map.proceduralmap.ProcMap;
 import codinggame.map.proceduralmap.ProcMapCell;
 import codinggame.map.proceduralmap.ProcMapChunk;
@@ -11,39 +13,32 @@ import codinggame.map.proceduralmap.ProcMapLayer;
 import codinggame.map.proceduralmap.entities.EntityData;
 import codinggame.map.proceduralmap.entities.rendering.EntityBatch;
 import codinggame.map.proceduralmap.entities.rendering.EntityHandler;
-import codinggame.map.proceduralmap.entities.rendering.ForEachable;
 import codinggame.map.proceduralmap.entities.rendering.ModelManager;
 import codinggame.map.renderer.g3d.Camera3D;
 import codinggame.map.renderer.g3d.DebugCamera3D;
 import codinggame.map.renderer.g3d.SkyboxRenderer;
 import codinggame.map.renderer.g3d.TerrainPicker;
 import codinggame.map.renderer.g3d.entities.AABBUtils.AABBBuilder;
-import codinggame.map.renderer.g3d.entities.Entity;
 import codinggame.map.renderer.g3d.entities.EntityModel;
-import codinggame.map.renderer.g3d.entities.renderer.EntityRenderer;
 import codinggame.map.renderer.g3d.terrain.RenderableChunk;
 import codinggame.objs.robots.Robot;
 import codinggame.states.GameState;
+import codinggame.utils.ColoredLogger;
 import codinggame.utils.INoise;
+import codinggame.utils.Timer;
 import codinggame.utils.textures.PackedTexture;
 import codinggame.utils.textures.TexturePacker;
 import com.lwjglwrapper.LWJGL;
 import com.lwjglwrapper.opengl.GLCalls;
 import com.lwjglwrapper.opengl.objects.Texture2D;
 import com.lwjglwrapper.opengl.objects.TextureData;
-import com.lwjglwrapper.opengl.objects.TexturedVAO;
 import com.lwjglwrapper.opengl.shaders.Shader;
 import com.lwjglwrapper.opengl.shaders.ShaderFile;
-import com.lwjglwrapper.opengl.shaders.uniforms.variables.UInt;
 import com.lwjglwrapper.opengl.shaders.uniforms.variables.UMat4;
-import com.lwjglwrapper.opengl.shaders.uniforms.variables.UVec3;
-import com.lwjglwrapper.opengl.shaders.uniforms.variables.UVec4;
-import com.lwjglwrapper.utils.Logger;
 import com.lwjglwrapper.utils.input.KeyBindings;
-import com.lwjglwrapper.utils.models.ModelGenerator;
-import com.lwjglwrapper.utils.models.OBJLoader;
 import java.awt.Point;
 import java.nio.ByteBuffer;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -54,6 +49,7 @@ import java.util.Set;
 import java.util.function.Consumer;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
+import org.joml.Vector4f;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
@@ -67,15 +63,15 @@ public class GameHandler {
     public static final float NOISE_SCALE = 0.2f;
     private GameState game;
     private ProcMap map;
-    private Shader shader, highlightShader;
+    private Shader shader;
     private Camera3D camera;
-    private UMat4 pvMatrix, h_pvMatrix;
+    private UMat4 pvMatrix;
     private KeyBindings keyBindings;
     private PackedTexture tileAtlas;
     private TerrainPicker picker;
     private MapTilesets tileset;
     
-    private Map<Point, RenderableChunk> renders;
+    private final Map<Point, RenderableChunk> renders;
     private Set<Point> circlePoints = generateCirclePoints(RENDER_DISTANCE);
     
     private final INoise noise;
@@ -89,16 +85,18 @@ public class GameHandler {
     private SkyboxRenderer skyboxRenderer;
     private Texture2D chooseMap;
     
-    private Map<TexturedVAO, Iterable<? extends Entity>> entities = new HashMap<>();
-    
     private AABBBuilder tileAABB = new AABBBuilder().xCenter(0f, 1f).yCenter(0f, 1f).zCenter(0f, 1f);
     
     private EntityHandler entityHandler;
+    private EntityLoader entityLoader;
+    
+    private EntityUpdateHandler entityUpdateHandler;
 
     public GameHandler(GameState game) {
         this.game = game;
         map = (ProcMap) game.getMapHandler().getMap();
         turfLayer = map.getMapLayer(GameMap.TURF_LAYER);
+        entityLoader = turfLayer.getEntityLoader();
         this.tileset = map.getTilesets();
         
         chooseMap = new Texture2D(TextureData.fromResource(GameHandler.class, "/chooseMap.png"));
@@ -108,11 +106,6 @@ public class GameHandler {
                 ShaderFile.fromResource(GameHandler.class, "/codinggame/map/renderer/g3d/fragment.glsl", GL20.GL_FRAGMENT_SHADER)
         );
         
-        highlightShader = new Shader(
-                ShaderFile.fromResource(GameHandler.class, "/codinggame/map/renderer/g3d/h_vertex.glsl", GL20.GL_VERTEX_SHADER),
-                ShaderFile.fromResource(GameHandler.class, "/codinggame/map/renderer/g3d/h_fragment.glsl", GL20.GL_FRAGMENT_SHADER)
-        );
-        
         picker = new TerrainPicker(LWJGL.window){
             @Override
             public float height(float x, float z) {
@@ -120,7 +113,6 @@ public class GameHandler {
             }
         };
         pvMatrix = new UMat4(shader, "pvMatrix");
-        h_pvMatrix = new UMat4(highlightShader, "pvMatrix");
 //        camera = new FirstPersonCamera(70.0f, LWJGL.window, 0.1f, 1000f);
         //camera.setSpeed(16f, 16f, 5f);
         keyBindings = new KeyBindings().defaultKeyBindings().debugKeyBindings();
@@ -131,8 +123,8 @@ public class GameHandler {
             public float getWorldHeight(float x, float z) {
                 return noise.getTriangleHeight(x, z);
             }
-        
         };
+        camera.getPosition().set(8, 8, 8);
         
         Map<Integer, MapTile> tiles = tileset.getTiles();
         TexturePacker packer = new TexturePacker(32, 32);
@@ -163,22 +155,29 @@ public class GameHandler {
         light = new Light(new Vector3f(0, 10000000f, 0), new Vector3f(1, 1, 1));
         skyboxRenderer = new SkyboxRenderer();
         
-        Iterable<? extends Entity> robots = game.getRobotHandler().getRobotList().values();
-        entities.put(ModelManager.ROBOT.getMesh(), robots);
-        
+        entityUpdateHandler = new EntityUpdateHandler(this, entityLoader);
         entityHandler.getEntities().put(ModelManager.ROBOT, game.getRobotHandler().getRobotList().values());
         entityHandler.getEntities().put((Consumer<EntityBatch> consumer) -> {
             Map<EntityModel, List> sortMap = new HashMap<>();
             for (RenderableChunk chunk : renders.values()) {
-                for (EntityData entity : chunk.getEntities().values()) {
+                Collection<Long> pointers = chunk.getEntities().values();
+                for (long pointer : pointers) {
+                    EntityData entity = entityLoader.getFromCache(pointer);
+                    EntityModel model = null;
                     if(entity.isTileEntity()) {
-                        EntityModel model = ModelManager.tileEntityModel(entity.getTileID());
-                        List batch = sortMap.get(model);
-                        if(batch == null) {
-                            sortMap.put(model, batch = new LinkedList<>());
-                        }
-                        batch.add(entity);
+                        model = ModelManager.tileEntityModel(entity.getSubTileID());
+                    } else if(entity.isTreeEntity()) {
+                        model = ModelManager.treeModel(entity.getSubTileID());
+                    } 
+                    if(entity.getType() == 4) {
+                        model = ModelManager.TEST;
+                        System.out.println("codinggame.handlers.GameHandler.<init>()");
                     }
+                    List batch = sortMap.get(model);
+                    if(batch == null) {
+                        sortMap.put(model, batch = new LinkedList<>());
+                    }
+                    batch.add(entity);
                 }
             }
             for (Map.Entry<EntityModel, List> entry : sortMap.entrySet()) {
@@ -187,10 +186,24 @@ public class GameHandler {
                 consumer.accept(new EntityBatch(model, (c) -> batch.forEach(c)));
             }
         });
+        
+        Timer.logger = (s, c) -> CodingGame.debug(s);
+        
+        EntityData testEntityData = entityLoader.emptyEntity();
+        testEntityData.setPosition(new Vector2f(8, 8));
+        testEntityData.setHeight(Float.NaN);
+        testEntityData.setType((byte) 4);
+        testEntityData.setSubTileID(MapTile.IRON_ORE);
+        
+        entityLoader.writeData(testEntityData.getEntityID(), testEntityData);
+        entityLoader.putToCache(testEntityData.getEntityID(), testEntityData);
+        
+        turfLayer.setEntityAt(8, 8, testEntityData.getEntityID());
     }
     
-    private Point lastCameraChunkPos;
     public void update(float delta) {
+        Timer.start();
+        
         camera.move(keyBindings, delta);
         if(keyBindings.anyReleased("polygon")) {
             wireframe = !wireframe;
@@ -207,15 +220,13 @@ public class GameHandler {
                 game.getChooseHandler().mouseReleased(ObjectChooseHandler.ChoosingObject.tile(x, z, turfLayer), picker.intersectionRayT);
             }
             if(LWJGL.mouse.mouseReleased(GLFW.GLFW_MOUSE_BUTTON_RIGHT)) {
-                turfLayer.setTileAt(x, z, MapTile.IRON_ORE);
             }
         }
-        
         Point cameraChunk = getChunk(camera);
+        
         Set<Point> usingChunkPositions = new HashSet<>();
         Set<RenderableChunk> usingChunks = new HashSet<>();
         
-        if(lastCameraChunkPos == null)  lastCameraChunkPos = new Point(cameraChunk);
         for (Point pt : circlePoints) {
             RenderableChunk chunk = renders.get(pt);
             if(chunk == null) {
@@ -234,8 +245,10 @@ public class GameHandler {
             usingChunkPositions.add(translated);
         }
         for (RenderableChunk chunk : renders.values()) {
-            Map<Point, EntityData> chunkEntities = chunk.getEntities();
-            for (EntityData entity : chunkEntities.values()) {
+            Set<Long> pointers = new HashSet<>(chunk.getEntities().values());
+            for (long pointer : pointers) {
+                EntityData entity = entityLoader.getFromCache(pointer);
+                if(entity == null)  continue;
                 if(entity.isTileEntity()) {
                     Vector3f translation = entity.get3DPosition().add(0.5f, 0.25f, 0.5f);
                     game.getChooseHandler().test(tileAABB, translation, ObjectChooseHandler.ChoosingObject.tileEntity(entity, turfLayer));
@@ -244,28 +257,28 @@ public class GameHandler {
             if(usingChunks.contains(chunk)) continue;
             chunk.unmark();
         }
-        lastCameraChunkPos = new Point(cameraChunk);
         
         turfLayer.saveChunksNotIn(usingChunkPositions);
         for (Robot robot : game.getRobotHandler().getRobotList().values()) {
             game.getChooseHandler().test(robot.getBoundBox(), new Vector3f(), ObjectChooseHandler.ChoosingObject.robot(robot));
         }
         game.getChooseHandler().endFrame();
-        for (Iterable<? extends Entity> batch : entities.values()) for (Entity entity : batch) {
-            entity.update(noise, delta);
-        }
-        ProcMapCell cell = (ProcMapCell) turfLayer.getTileAt(random.nextInt(16), random.nextInt(16));
-        TileUpdateHandler.updateCell(cell);
+        entityUpdateHandler.update(delta, renders);
+        int x = random.nextInt(16);
+        int y = random.nextInt(16);
+        TileUpdateHandler.updateCell(turfLayer, x, y);
         EntityData eCell = turfLayer.getEntityAt(random.nextInt(16), random.nextInt(16));
         if(eCell != null)   TileUpdateHandler.updateEntityCell(eCell);
+        
+        
+        Timer.stop();
     }
     
     public static final Random random = new Random();
     
     boolean wireframe = false;
-    private Map<Integer, TexturedVAO> tileModels = new HashMap<>();
-    private ModelGenerator mg = new ModelGenerator(new OBJLoader(0, 2, 1));
     public void render() {
+        Timer.start();
         GLCalls.enable(GL11.GL_BLEND, GL11.GL_CULL_FACE);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         GL11.glCullFace(GL11.GL_FRONT);
@@ -274,23 +287,51 @@ public class GameHandler {
         shader.bind();
         pvMatrix.load(camera.getCombinedMatrix());
         GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, wireframe? GL11.GL_LINE : GL11.GL_FILL);
-        tileAtlas.bind(0);
-        new UInt(shader, "tileAtlas").load(0);
-        new UVec4(shader, "mixColor").load(0f, 0f, 0f, 0f);
+        shader.loadUniformVariable("tileAtlas", tileAtlas.bind(0));
+        shader.loadUniformVariable("chooseMap", chooseMap.bind(1));
         lightStruct.set(light);
         
         Point cameraChunk = getChunk(camera);
         
+        int mixColor_location = shader.loadUniformVariable("mixColor", new Vector4f(1f, 1f, 1f, 0f)); 
         for (Map.Entry<Point, RenderableChunk> entry : renders.entrySet()) {
             Point pt = entry.getKey();
             pt = new Point(pt.x + cameraChunk.x, pt.y + cameraChunk.y);
             RenderableChunk chunk = entry.getValue();
-            new UVec3(shader, "translation").load(pt.x * ProcMapChunk.CHUNK_SIZE, 0, pt.y * ProcMapChunk.CHUNK_SIZE);
+            shader.loadUniformVariable("translation", new Vector3f(pt.x * ProcMapChunk.CHUNK_SIZE, 0, pt.y * ProcMapChunk.CHUNK_SIZE));
             chunk.render();
         }
 
+        shader.loadUniformVariable(mixColor_location, new Vector4f(1f, 1f, 1f, 1f));
         ObjectChooseHandler.ChoosingObject choosingObject = game.getChooseHandler().getChoosingObject();
         ObjectChooseHandler.Choosable choosing = choosingObject.getObject();
+        if(picker.currentTerrainPoint != null) {
+            int tileX = (int) Math.floor(picker.currentTerrainPoint.x);
+            int tileY = (int) Math.floor(picker.currentTerrainPoint.z);
+            
+            int inChunkX = tileX % ProcMapChunk.CHUNK_SIZE;
+            if(inChunkX < 0)   inChunkX += ProcMapChunk.CHUNK_SIZE;
+            int inChunkY = tileY % ProcMapChunk.CHUNK_SIZE;
+            if(inChunkY < 0)   inChunkY += ProcMapChunk.CHUNK_SIZE;
+            
+            int chunkX = (int) Math.floor((double) tileX / ProcMapChunk.CHUNK_SIZE);
+            int chunkY = (int) Math.floor((double) tileY / ProcMapChunk.CHUNK_SIZE);
+            
+            shader.loadUniformVariable("translation", new Vector3f(chunkX * ProcMapChunk.CHUNK_SIZE, 0, chunkY * ProcMapChunk.CHUNK_SIZE));
+            
+            chunkX -= cameraChunk.x;
+            chunkY -= cameraChunk.y;
+            
+            shader.loadUniformVariable("mixColor", new Vector4f(1f, 1f, 1f, 0.25f));
+            
+            RenderableChunk chunk = renders.get(new Point(chunkX, chunkY));
+            
+            GLCalls.disable(GL11.GL_DEPTH_TEST);
+            chunk.renderTileAt(inChunkX, inChunkY);
+            GLCalls.enable(GL11.GL_DEPTH_TEST);
+            CodingGame.debug(chunkX, chunkY, inChunkX, inChunkY);
+        }
+        
         if(choosing instanceof ProcMapCell) {
             int tileX = choosingObject.getX();
             int tileY = choosingObject.getY();
@@ -303,29 +344,37 @@ public class GameHandler {
             int chunkX = (int) Math.floor((double) tileX / ProcMapChunk.CHUNK_SIZE);
             int chunkY = (int) Math.floor((double) tileY / ProcMapChunk.CHUNK_SIZE);
             
-            new UVec3(shader, "translation").load(chunkX * ProcMapChunk.CHUNK_SIZE, 0, chunkY * ProcMapChunk.CHUNK_SIZE);
+            shader.loadUniformVariable("translation", new Vector3f(chunkX * ProcMapChunk.CHUNK_SIZE, 0, chunkY * ProcMapChunk.CHUNK_SIZE));
             
             chunkX -= cameraChunk.x;
             chunkY -= cameraChunk.y;
             
-            new UVec4(shader, "mixColor").load(1f, 1f, 1f, 1f);
+            shader.loadUniformVariable("mixColor", new Vector4f(1f, 1f, 1f, 0.5f));
+            
             RenderableChunk chunk = renders.get(new Point(chunkX, chunkY));
             
             GLCalls.disable(GL11.GL_DEPTH_TEST);
             chunk.renderTileAt(inChunkX, inChunkY);
             GLCalls.enable(GL11.GL_DEPTH_TEST);
-            Logger.logln(chunkX, chunkY, inChunkX, inChunkY);
+            CodingGame.debug(chunkX, chunkY, inChunkX, inChunkY);
+            game.getChooseHandler().debug();
         }
         
         tileAtlas.unbind();
         GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
         shader.unbind();
         
-        //renderer.render(camera, entities);
-        //renderer.render(camera, tileEntities);
-        
         entityHandler.render(camera);
+        long time = Timer.stop(ColoredLogger.Color16.FG_RED);
+        
+        avg = (time + avg * frames) / (frames + 1);
+        frames++;
+        
+        CodingGame.debug(time, avg, frames);
     }
+    
+    int frames;
+    long avg;
     
     private static Set<Point> generateCirclePoints(int radius) {
         Set<Point> set = new HashSet<>();
@@ -369,6 +418,14 @@ public class GameHandler {
 
     public INoise getNoise() {
         return noise;
+    }
+
+    public ProcMapLayer getTurfLayer() {
+        return turfLayer;
+    }
+
+    public ProcMap getMap() {
+        return map;
     }
     
 }
